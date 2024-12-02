a9d78c5f06c79ea23f220823ec2f2955463f20d0
Caden Newton <cnewton@ucdavis.edu>
2024-12-01 22:46:48 -0800

    Check existing data when loading data from csv

--- npsat_manager/load_data.py
+++ npsat_manager/load_data.py
@@ -12,11 +12,14 @@ data_folder = os.path.join(settings.BASE_DIR, "npsat_manager", "data")
 
 
 def load_all(mantis_port_number=5941):
+    load_all_data()
+    load_mantis_server(mantis_port_number=mantis_port_number)
+    load_system_admin_bot()
+
+def load_all_data():
     load_crops()
     load_regions()
     load_scenarios()
-    load_mantis_server(mantis_port_number=mantis_port_number)
-    load_system_admin_bot()
 
 
 def load_system_admin_bot():
@@ -61,9 +64,21 @@ def load_crops(
     """
 
     # add ALL Other Crops first
-    models.Crop.objects.create(
-        name="All Other Crops", crop_type=models.Crop.ALL_OTHER_CROPS
-    )
+    if models.Crop.objects.filter(name="All Other Crops").count() == 0:
+        models.Crop.objects.create(
+            name="All Other Crops", crop_type=models.Crop.ALL_OTHER_CROPS
+        )
+    
+    for crop in models.Crop.objects.all():
+        crop.active_in_mantis = False
+        crop.save()
+
+    try:
+        all_other_crops = models.Crop.objects.get(name="All Other Crops")
+        all_other_crops.active_in_mantis = True
+        all_other_crops.save()
+    except models.Crop.DoesNotExist:
+        pass
 
     with open(crop_csv, "r") as csv_data:
         crop_list = csv.DictReader(csv_data)
@@ -71,7 +86,13 @@ def load_crops(
         for record in crop_list:
             # make sure both the GNLM and SWAT variants exist
             try:
-                swat_crop = models.Crop.objects.get(swat_code=record[swat_id_field])
+                swat_crop = models.Crop.objects.get(
+                    swat_code=record[swat_id_field], 
+                    name=record[swat_name_field],
+                    crop_type=models.Crop.SWAT_CROP,
+                )
+                swat_crop.active_in_mantis = True
+                swat_crop.save()
             except models.Crop.DoesNotExist:
                 swat_crop = models.Crop(
                     name=record[swat_name_field],
@@ -81,7 +102,13 @@ def load_crops(
                 swat_crop.save()
 
             try:
-                gnlm_crop = models.Crop.objects.get(caml_code=record[gnlm_id_field])
+                gnlm_crop = models.Crop.objects.get(
+                    caml_code=record[gnlm_id_field],
+                    name=record[gnlm_name_field],
+                    crop_type=models.Crop.GNLM_CROP,
+                )
+                gnlm_crop.active_in_mantis = True
+                gnlm_crop.save()
             except models.Crop.DoesNotExist:
                 gnlm_crop = models.Crop(
                     name=record[gnlm_name_field],
@@ -245,15 +272,33 @@ def load_spec_regions(json_file, field_map, region_type, mantis_id_loader=None):
     with open(json_file, "r") as input_data:
         geojson = input_data.readlines()
 
+    if mantis_id_loader:
+        regions = models.Region.objects.filter(region_type=region_type)
+        for region in regions:
+            region.active_in_mantis = False
+            region.save()
+
     for record in geojson:
         # make a Python version of the JSON record
         python_data = json.loads(record)
+
+        try:
+            if mantis_id_loader:
+                filter_criteria = {}
+                for ( fm ) in field_map:
+                    filter_criteria[fm[1]] = python_data["properties"][fm[0]]
+
+                existing_region = models.Region.objects.get(mantis_id=mantis_id_loader(python_data["properties"]), **filter_criteria)
+                existing_region.active_in_mantis = True
+                existing_region.save()
+                continue
+        except models.Region.DoesNotExist:
+            pass
+
         region = models.Region()  # make a new region object
         region.geometry = record  # save the whole JSON record as the geometry we'll send to the browser in the future
 
-        for (
-            fm
-        ) in field_map:  # apply all the attributes to the region based on the field map
+        for ( fm ) in field_map:  # apply all the attributes to the region based on the field map
             value = python_data["properties"][fm[0]]
             if hasattr(
                 region, fm[1]
@@ -263,6 +308,7 @@ def load_spec_regions(json_file, field_map, region_type, mantis_id_loader=None):
 
         if mantis_id_loader:
             region.mantis_id = mantis_id_loader(python_data["properties"])
+
         region.save()  # save it with the new attributes
 
 
@@ -312,7 +358,25 @@ def enable_scenario_dev_data():
     scenarios_csv = os.path.join(data_folder, "scenarios", "MantisScenariosNames.csv")
     with open(scenarios_csv, "r") as scenario_data:
         scenario_list = csv.DictReader(scenario_data)
+        for scenario in models.Scenario.objects.all():
+            scenario.active_in_mantis = False
+            scenario.save()
+
         for scenario in scenario_list:
+            try:
+                existing_scenario = models.Scenario.objects.get(
+                    name=scenario["User friendly name"],
+                    mantis_id=scenario["Code name"],
+                    description=scenario["Short description"],
+                    long_description=scenario["Long description"],
+                    external_url=scenario["url"],
+                )
+                existing_scenario.active_in_mantis = True
+                existing_scenario.save()
+                continue
+            except models.Scenario.DoesNotExist:
+                pass
+
             category = scenario["Category"]
             crop_code_field = None
             if category == "Load Scenario":
