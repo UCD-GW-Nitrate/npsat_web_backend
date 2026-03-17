80a6b330e9743e9e1b9e9ef127e064de2456bc64
Sreya Mathew <134162911+tiramisuuuuuuu@users.noreply.github.com>
2026-03-17 05:56:30 -0700

    Dev (#68)
    * Forward user feedback to admins via email

--- npsat_backend/urls.py
+++ npsat_backend/urls.py
@@ -62,6 +62,7 @@ urlpatterns = [
     re_path(r"^api/user/verify/", views.SendVerificationEmail.as_view()),
     re_path(r"^unauthorized-verify/", views.SendUnauthenticatedVerificationEmail.as_view()),
     re_path(r"^verify-code/", views.VerifyCode.as_view()),
+    re_path(r"^send-feedback/", views.SendUserFeedback.as_view()),
     re_path(r"^register-user/", views.CreateUserView.as_view()),
     re_path(
         r"^api-token-auth/", views.CustomAuthToken.as_view()
